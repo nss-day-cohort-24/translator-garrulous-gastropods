c10ff4534fe7fd85875e35d5f570d1180161b72a
ROHINI CHAVAN <rohinichavan@ROHINIs-MacBook-Pro.local>
2018-02-05 14:59:44 -0600

    started franch.js file

---
+++ js/french.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log("french.js");
+
+const frenchModule = {
+    "wishing": "souhaitant",
+    "the":"le",
+    "sweetest": "plus doux", 
+    "happiest": "plus heureux",
+    "day": "jour",
+    "to": "à",
+    "my":"mon",
+    "valentine": "valentin", 
+}
