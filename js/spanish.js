b05c5ca755078a5f457d340323df1becd46edc9c
Million Yemane <wedi_ngus@yahoo.com>
2018-02-05 20:24:52 -0600

    second commit

--- js/spanish.js
+++ js/spanish.js
@@ -1,14 +1,8 @@
 "use strict";
 console.log("welcome to our site")
 const spanishModule={
-    "wishing": "deseando",
-    "the":"el",
-    "sweetest":"más dulce",
-   "happiest":"más feliz",
-    "day":"dia",
-    "to":"a",
-    "my":"mi",
-    "forever":"Siempre",
-    "valentine":"Enamorado.",
-}
+    "Happy":"contento",
+    "valentines":"San Valentin",
+    "day":"dia"
 
+};
